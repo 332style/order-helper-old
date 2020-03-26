5354f67f314eed1821932d566b5d7d6955031712
Mickey <Micky40086@gmail.com>
2020-03-26 17:51:25 +0800

    init: sign_in page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <el-row>
-      <el-button icon="el-icon-search" circle></el-button>
-      <el-button type="primary" icon="el-icon-edit" circle></el-button>
-      <el-button type="success" icon="el-icon-check" circle></el-button>
-      <el-button type="info" icon="el-icon-message" circle></el-button>
-      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle></el-button>
-    </el-row>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -32,6 +23,5 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/user/sign_in.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-container class="container">
+    <!-- <el-header>Header</el-header> -->
+    <el-main>
+      <div class="user-form">
+        <el-input v-model="email" placeholder="请输入帳號"></el-input>
+        <el-input
+          placeholder="请输入密碼"
+          v-model="password"
+          show-password
+        ></el-input>
+        <el-button type="primary">登入</el-button>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+<style lang="scss">
+.container {
+  main {
+    display: flex;
+    justify-content: center;
+    .user-form {
+      max-width: 350px;
+      border: 1px solid rgba(0, 0, 0, 0.6);
+      border-radius: 5px;
+      padding: 20px;
+      .el-input {
+        margin: 10px 0;
+      }
+      .el-button {
+        margin-top: 10px;
+      }
+    }
+  }
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  }
+};
+</script>
